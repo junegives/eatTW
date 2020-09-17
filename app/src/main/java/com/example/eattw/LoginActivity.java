2c84963b5850795d06057174e53964229a9285bf
JUNE <jjhttoz@naver.com>
2020-09-17 23:52:02 +0900

    로그인, 회원가입 기능 추가

---
+++ app/src/main/java/com/example/eattw/LoginActivity.java
@@ -0,0 +1,71 @@
+package com.example.eattw;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText et_email_login;
+    private EditText et_password_login;
+    private Button btn_login;
+    private Button btn_signup_go;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        et_email_login = findViewById(R.id.et_email_login);
+        et_password_login = findViewById(R.id.et_password_login);
+        btn_login = findViewById(R.id.btn_login);
+        btn_signup_go = findViewById(R.id.btn_signup_go);
+
+        btn_login.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //trim() = 공백 제거 함수
+                String email = et_email_login.getText().toString().trim();
+                String password = et_password_login.getText().toString().trim();
+
+                firebaseAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                //성공했을 때
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                    startActivity(intent);
+                                }
+                                //실패했을 때
+                                else{
+                                    Toast.makeText(LoginActivity.this, "이메일 혹은 패스워드 입력이 잘못되었습니다.", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+        btn_signup_go.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), SignupActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/eattw/SignupActivity.java
@@ -0,0 +1,81 @@
+package com.example.eattw;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignupActivity extends AppCompatActivity {
+
+    private EditText et_email_signup;
+    private EditText et_password_signup;
+    private EditText et_password_check_signup;
+    private Button btn_signup;
+    private Button btn_login_go;
+    FirebaseAuth firebaseAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+
+        firebaseAuth = firebaseAuth.getInstance();
+
+        et_email_signup = findViewById(R.id.et_email_signup);
+        et_password_signup = findViewById(R.id.et_password_signup);
+        et_password_check_signup = findViewById(R.id.et_password_check_signup);
+        btn_signup = findViewById(R.id.btn_signup);
+        btn_login_go = findViewById(R.id.btn_login_go);
+
+        btn_signup.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                final String email = et_email_signup.getText().toString().trim();
+                final String password = et_password_signup.getText().toString().trim();
+                final String password_check = et_password_check_signup.getText().toString().trim();
+                //trim() = 공백인 부분을 제거함
+
+                if(!password.equals(password_check)){
+                    Toast.makeText(SignupActivity.this, "비밀번호를 확인하세요", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    firebaseAuth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    //성공했을 때
+                                    if(task.isSuccessful()){
+                                        Toast.makeText(SignupActivity.this, "회원가입 성공", Toast.LENGTH_LONG).show();
+                                        Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                        startActivity(intent);
+                                        finish();
+                                    }
+                                    //실패했을 때
+                                    else{
+                                        Toast.makeText(SignupActivity.this, "회원가입 실패", Toast.LENGTH_LONG).show();
+                                        return;
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+
+        btn_login_go.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
