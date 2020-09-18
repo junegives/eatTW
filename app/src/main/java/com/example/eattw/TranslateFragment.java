103da7a4cc3e77eec327dc77aa200e29529eb441
JUNE <jjhttoz@naver.com>
2020-09-19 05:57:58 +0900

    구글 비전 api

--- app/src/main/java/com/example/eattw/TranslateFragment.java
+++ app/src/main/java/com/example/eattw/TranslateFragment.java
@@ -1,9 +1,11 @@
 package com.example.eattw;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,6 +14,7 @@ import androidx.fragment.app.Fragment;
 public class TranslateFragment  extends Fragment {
 
     private View view;
+    private Button btn_picture_go;
 
 
     @Nullable
@@ -20,6 +23,14 @@ public class TranslateFragment  extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         view = inflater.inflate(R.layout.fragment_translate, container, false);
+
+        btn_picture_go = (Button)view.findViewById(R.id.btn_picture_go);
+        btn_picture_go.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                Intent intent = new Intent(getActivity(), PicTransActivity.class);
+                startActivity(intent);
+            }
+        });
         return view;
     }
 }
