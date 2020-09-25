22fd8d128ba328e47e3078edd595351436fc7b3b
JUNE <jjhttoz@naver.com>
2020-09-26 01:46:32 +0900

    ML Kit API를 사용한 번역 기능 추가

---
+++ app/src/main/java/com/example/eattw/ImgTranslateActivity.java
@@ -0,0 +1,196 @@
+package com.example.eattw;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.MediaScannerConnection;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
+
+import com.example.eattw.Helper.GraphicOverlay;
+import com.example.eattw.Helper.TextGraphic;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionCloudTextRecognizerOptions;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
+import com.wonderkiln.camerakit.CameraView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+
+import dmax.dialog.SpotsDialog;
+
+public class ImgTranslateActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private AlertDialog waitingDialog;
+    private GraphicOverlay graphicOverlay;
+    private Button btn_ReCapture;
+
+    //권한 요청을 할 때 발생하는 창에 대한 결과값을 받기 위해 지정해주는 정수
+    private final int PERMISSION_CODE = 12;
+
+    //요청할 권한을 배열로 저장 ( 카메라, 외부저장소 읽기, 외부저장소 쓰기)
+    private String[] PERMISSIONS_CAMERA = {
+            Manifest.permission.CAMERA
+    };
+
+    //onActivityResult를 위해 사용할 requestCode
+    private static final int CAPTURE = 1;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_img_translate);
+
+        //권한 설정되어 있는지 먼저 확인
+        //권한이 허용되어있지 않다면 권한요청
+        if(!hasPermissions(ImgTranslateActivity.this, PERMISSIONS_CAMERA)){
+            ActivityCompat.requestPermissions(ImgTranslateActivity.this, PERMISSIONS_CAMERA, PERMISSION_CODE);
+        }
+
+        //권한이 허용되어 있다면 다음 화면 진행
+        else{
+            takePhoto();
+        }
+
+        waitingDialog = new SpotsDialog.Builder()
+                .setCancelable(false)
+                .setMessage("잠시만 기다려주세요")
+                .setContext(this)
+                .build();
+
+        imageView = (ImageView)findViewById(R.id.Image_view);
+        graphicOverlay = (GraphicOverlay)findViewById(R.id.graphic_overlay);
+        btn_ReCapture = (Button)findViewById(R.id.btn_recapture);
+
+        btn_ReCapture.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                takePhoto();
+                graphicOverlay.clear();
+            }
+        });
+    }
+
+    public boolean hasPermissions(Context context, String... permissions){
+        if(context != null && permissions != null){
+            for (String permission : permissions){
+                if(ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private void takePhoto(){
+        //사진을 찍기 위하여 설정합니다.
+        Log.d("이미지", "takePhoto()함수 실행");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imageBitmap);
+            recognizeText(imageBitmap);
+        }
+    }
+
+    private void recognizeText(Bitmap bitmap) {
+        waitingDialog.show();
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+
+        FirebaseVisionCloudTextRecognizerOptions options =
+                new FirebaseVisionCloudTextRecognizerOptions.Builder()
+                        .setLanguageHints(Arrays.asList("zh"))
+                        .build();
+
+        FirebaseVisionTextRecognizer textRecognizer = FirebaseVision.getInstance()
+                .getCloudTextRecognizer(options);
+
+        textRecognizer.processImage(image)
+                .addOnSuccessListener(new OnSuccessListener<FirebaseVisionText>() {
+                    @Override
+                    public void onSuccess(FirebaseVisionText firebaseVisionText) {
+                        drawTextResult(firebaseVisionText);
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("EDMT_ERROR", e.getMessage());
+
+            }
+        });
+    }
+
+    private void drawTextResult(FirebaseVisionText firebaseVisionText) {
+        List<FirebaseVisionText.TextBlock> blocks = firebaseVisionText.getTextBlocks();
+        waitingDialog.dismiss();
+
+        if(blocks.size() == 0)
+        {
+            Toast.makeText(this, "텍스트를 찾지 못하였습니다.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        graphicOverlay.clear();
+
+        for(int i = 0; i < blocks.size(); i++)
+        {
+            Log.d("blocks.size()", blocks.size()+"");
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for(int j=0; j<lines.size(); j++)
+            {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+
+                for(int k = 0; k<elements.size(); k++)
+                {
+                    TextGraphic textGraphic = new TextGraphic(graphicOverlay, elements.get(k));
+                    graphicOverlay.add(textGraphic);
+                    Log.d("textGraphic", textGraphic+"");
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/eattw/TranslateFragment.java
+++ app/src/main/java/com/example/eattw/TranslateFragment.java
@@ -2,20 +2,64 @@ package com.example.eattw;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+import java.util.ArrayList;
 
 public class TranslateFragment  extends Fragment {
 
     private View view;
     private Button btn_picture_go;
 
+    private Button input_language;
+    private ImageButton swap_language;
+    private Button output_language;
+
+    private LinearLayout input_spinner;
+    private Button btn_input_korean;
+    private Button btn_input_chinese;
+    private Button btn_input_english;
+
+    private LinearLayout output_spinner;
+    private Button btn_output_korean;
+    private Button btn_output_chinese;
+    private Button btn_output_english;
+
+    private ImageButton input_pronounce;
+    private ImageButton input_clear;
+
+    private EditText input_translate;
+
+    private ImageButton output_pronounce;
+    private ImageButton output_save;
+
+    private TextView output_translate;
+
+    String sourceText = null;
+    String input = null;
+    String output = null;
 
     @Nullable
     @Override
@@ -24,13 +68,201 @@ public class TranslateFragment  extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_translate, container, false);
 
+
         btn_picture_go = (Button)view.findViewById(R.id.btn_picture_go);
         btn_picture_go.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
-                Intent intent = new Intent(getActivity(), PicActivity.class);
+                Intent intent = new Intent(getActivity(), ImgTranslateActivity.class);
                 startActivity(intent);
             }
         });
+
+        input_spinner = (LinearLayout)view.findViewById(R.id.input_spinner);
+        output_spinner = (LinearLayout)view.findViewById(R.id.output_spinner);
+
+        input_language = (Button)view.findViewById(R.id.input_language);
+        input_language.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input_spinner.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_input_korean = (Button)view.findViewById(R.id.btn_input_korean);
+        btn_input_korean.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input_spinner.setVisibility(View.GONE);
+                input_language.setText("한국어");
+            }
+        });
+        btn_input_chinese = (Button)view.findViewById(R.id.btn_input_chinese);
+        btn_input_chinese.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input_spinner.setVisibility(View.GONE);
+                input_language.setText("중국어");
+            }
+        });
+        btn_input_english = (Button)view.findViewById(R.id.btn_input_english);
+        btn_input_english.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input_spinner.setVisibility(View.GONE);
+                input_language.setText("영어");
+            }
+        });
+
+        swap_language = (ImageButton)view.findViewById(R.id.swap_language);
+        swap_language.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input = input_language.getText().toString();
+                output = output_language.getText().toString();
+                input_language.setText(output);
+                output_language.setText(input);
+                input = input_language.getText().toString();
+                output = output_language.getText().toString();
+            }
+        });
+        output_language = (Button)view.findViewById(R.id.output_language);
+        output_language.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                output_spinner.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btn_output_korean = (Button)view.findViewById(R.id.btn_output_korean);
+        btn_output_korean.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                output_spinner.setVisibility(View.GONE);
+                output_language.setText("한국어");
+            }
+        });
+        btn_output_chinese = (Button)view.findViewById(R.id.btn_output_chinese);
+        btn_output_chinese.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                output_spinner.setVisibility(View.GONE);
+                output_language.setText("중국어");
+            }
+        });
+        btn_output_english = (Button)view.findViewById(R.id.btn_output_english);
+        btn_output_english.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                output_spinner.setVisibility(View.GONE);
+                output_language.setText("영어");
+            }
+        });
+
+        input_pronounce = (ImageButton)view.findViewById(R.id.btn_inputpronounce);
+        input_pronounce.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        input_clear = (ImageButton)view.findViewById(R.id.input_clear);
+        input_clear.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                input_translate.setText("");
+            }
+        });
+
+        input_translate = (EditText)view.findViewById(R.id.input_translate);
+        input_translate.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        input_translate.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        input_translate.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                sourceText = input_translate.getText().toString();
+                input = input_language.getText().toString();
+                output = output_language.getText().toString();
+                getLanguageCode();
+                return false;
+            }
+        });
+
+        output_pronounce = (ImageButton)view.findViewById(R.id.btn_outputpronounce);
+        output_pronounce.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            }
+        });
+        output_save = (ImageButton)view.findViewById(R.id.btn_outputsave);
+        output_save.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        output_translate = (TextView)view.findViewById(R.id.output_translate);
+
         return view;
+    }
+
+    private void translateText(int langCode_i, int langCode_o){
+        FirebaseTranslatorOptions options = new FirebaseTranslatorOptions.Builder()
+                .setSourceLanguage(langCode_i)
+                .setTargetLanguage(langCode_o)
+                .build();
+
+        final FirebaseTranslator translator =
+                FirebaseNaturalLanguage.getInstance().getTranslator(options);
+
+        FirebaseModelDownloadConditions conditions = new FirebaseModelDownloadConditions.Builder()
+                .build();
+
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                translator.translate(sourceText).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        output_translate.setText(s);
+                    }
+                });
+            }
+        });
+    }
+
+    private void getLanguageCode(){
+        int langCode_i;
+        int langCode_o;
+
+        switch(input){
+            case "한국어" :
+                langCode_i = FirebaseTranslateLanguage.KO;
+                break;
+            case "중국어" :
+                langCode_i = FirebaseTranslateLanguage.ZH;
+                break;
+            case "영어" :
+                langCode_i = FirebaseTranslateLanguage.EN;
+                break;
+            default:
+                langCode_i = 0;
+        }
+
+        switch(output){
+            case "한국어" :
+                langCode_o = FirebaseTranslateLanguage.KO;
+                break;
+            case "중국어" :
+                langCode_o = FirebaseTranslateLanguage.ZH;
+                break;
+            case "영어" :
+                langCode_o = FirebaseTranslateLanguage.EN;
+                break;
+            default:
+                langCode_o = 0;
+        }
+
+        translateText(langCode_i, langCode_o);
     }
 }
